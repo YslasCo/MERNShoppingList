1a7495dfb735f526a066a68af4da36616803d4d4
Jared Yslas <jryslas@gmail.com>
2019-02-25 20:35:05 -0800

    db connection needs work.  Current error: MongoParseError: Invalid connection string, video 2 @ 11:58.  Also, attempting to get mongo version, but can't find it...yet...

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb:JaredYslas:yslas987654@mernshoppinglist-shard-00-00-yusn2.mongodb.net:27017,mernshoppinglist-shard-00-01-yusn2.mongodb.net:27017,mernshoppinglist-shard-00-02-yusn2.mongodb.net:27017/test?ssl=true&replicaSet=mernShoppingList-shard-0&authSource=admin&retryWrites=true'
+}
